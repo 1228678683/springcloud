4b19bb74a1ed4f71454a96793852d43da5b371ae
h1634668150 <1634668150@qq.com>
2019-01-22 13:22:33 +0800

    默默

---
+++ lagou_web_consumer/src/main/java/com/lagou/web_consumer_8083.java
@@ -0,0 +1,17 @@
+package com.lagou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+//@MapperScan("com.lagou.company.mapper")//创建自己的包
+@EnableEurekaClient   //eureka客户端
+@EnableFeignClients    //添加feign的支持
+public class web_consumer_8083 {
+    public static void main(String[] args) {
+        SpringApplication.run(web_consumer_8083.class);
+    }
+}
